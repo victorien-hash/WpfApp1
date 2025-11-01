f498f5b4aa09d0c9e5ec4f9d0bc9e91bd4fe65af
victorien-hash <victorienfotso@gmail.com>
2025-10-31 21:41:01 -0400

    ajout de deux pages

---
+++ WpfApp1/Modifier_info_film.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour Modifier_info_film.xaml
+    /// </summary>
+    public partial class Modifier_info_film : Window
+    {
+        public Modifier_info_film()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp1/gerer_compte_admin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour gerer_compte_admin.xaml
+    /// </summary>
+    public partial class gerer_compte_admin : Window
+    {
+        public gerer_compte_admin()
+        {
+            InitializeComponent();
+        }
+    }
+}
