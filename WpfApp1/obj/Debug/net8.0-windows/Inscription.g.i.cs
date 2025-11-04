72e1075d23932254d925acf1ba90f241c6095581
victorien-hash <victorienfotso@gmail.com>
2025-11-04 00:07:04 -0500

    ajout de l acceuil

--- WpfApp1/obj/Debug/net8.0-windows/Inscription.g.i.cs
+++ WpfApp1/obj/Debug/net8.0-windows/Inscription.g.i.cs
@@ -53,7 +53,7 @@ namespace WpfApp1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp1;V1.0.0.0;component/inscription.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp1;component/inscription.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Inscription.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- WpfApp1/obj/Debug/net8.0-windows/VisionnerFilm.g.i.cs
+++ WpfApp1/obj/Debug/net8.0-windows/VisionnerFilm.g.i.cs
@@ -53,7 +53,7 @@ namespace WpfApp1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp1;V1.0.0.0;component/visionnerfilm.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp1;component/visionnerfilm.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\VisionnerFilm.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
