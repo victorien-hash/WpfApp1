a2e5ed3c7ded9a1b370e2d8afd12fd69168e4209
victorien-hash <victorienfotso@gmail.com>
2025-11-30 01:38:17 -0500

    correction de la barre de recherche et remplissage du dossier des interfaces

--- WpfApp1/obj/Debug/net8.0-windows/ProfilUtilisateur.g.i.cs
+++ WpfApp1/obj/Debug/net8.0-windows/ProfilUtilisateur.g.i.cs
@@ -52,7 +52,7 @@ namespace WpfApp1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp1;component/profilutilisateur.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp1;V1.0.0.0;component/profilutilisateur.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ProfilUtilisateur.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
